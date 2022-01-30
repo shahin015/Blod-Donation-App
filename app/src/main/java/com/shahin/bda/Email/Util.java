8c1ac34716598755f4c98d21b894e3d1a6c61e1c
Shahin <admin@shahinrhaman30.com>
2022-01-30 21:13:55 +0600

    from Shahin Rahman

---
+++ app/src/main/java/com/shahin/bda/Email/Util.java
@@ -0,0 +1,7 @@
+package com.shahin.bda.Email;
+
+public class Util {
+    // Add your own email and password over here.
+    public  static  final String EMAIL = "blooddonationmobileapp@gmail.com";
+    public static final String PASSWORD = "ADD YOUR OWN PASSWORD HERE";
+}
